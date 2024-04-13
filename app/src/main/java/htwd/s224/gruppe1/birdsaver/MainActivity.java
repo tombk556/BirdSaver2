d2e371686bb0841da7b89335420d756e8b8df86f
Tom Bischopink <s87622@informatik.htw-dresden.de>
2024-04-13 09:24:04 +0200

    added new button

--- app/src/main/java/htwd/s224/gruppe1/birdsaver/MainActivity.java
+++ app/src/main/java/htwd/s224/gruppe1/birdsaver/MainActivity.java
@@ -1,6 +1,8 @@
 package htwd.s224.gruppe1.birdsaver;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,17 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        textView = (TextView) findViewById(R.id.textView);
+
+    }
+
+    public void updateText(View view){
+        textView.setText("Button clicked");
     }
 }
