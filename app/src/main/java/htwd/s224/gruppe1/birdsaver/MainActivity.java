4c5bdbb4df47980cfa60ab6bce64c5d7bf0a723c
Selina Natschke <natschke.selina@gmail.com>
2024-04-15 22:14:32 +0200

    added first layout sketch

--- app/src/main/java/htwd/s224/gruppe1/birdsaver/MainActivity.java
+++ app/src/main/java/htwd/s224/gruppe1/birdsaver/MainActivity.java
@@ -2,27 +2,17 @@ package htwd.s224.gruppe1.birdsaver;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        textView = (TextView) findViewById(R.id.textView);
-
     }
 
-    public void updateText(View view){
-        textView.setText("Button clicked");
+    public void submitIpAddress(View view){
     }
 }
